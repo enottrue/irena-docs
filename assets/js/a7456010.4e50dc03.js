8260f09fe902b22a93d2685d18e7e73bb86ce472
enottrue <enottrue@gmail.com>
2025-05-13 10:55:36 +0300

    deploy: build static site to gh-pages

---
+++ assets/js/a7456010.4e50dc03.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkirena_docs=self.webpackChunkirena_docs||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
